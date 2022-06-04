06d03d0f251ededb90ce7929b4a2f22fefe6e7fa
Manuel Cassiani <manuel.cassiani@poincenot.com>
2022-06-04 10:10:33 -0300

    Modificar primer FE y primer YML

--- src/completo/912_FE_desborde.r
+++ src/completo/912_FE_desborde.r
@@ -415,6 +415,43 @@ TendenciaYmuchomas  <- function( dataset, cols, ventana=6, tendencia=TRUE, minim
 
 }
 #------------------------------------------------------------------------------
+#calcula la tendencia de las variables cols de los ultimos 12 meses
+#la tendencia es la pendiente de la recta que ajusta por cuadrados minimos
+#La funcionalidad de ratioavg es autoria de  Daiana Sparta,  UAustral  2021
+
+TendenciaYmuchomasDOS  <- function( dataset, cols, ventana=12, tendencia=TRUE, minimo=TRUE, maximo=TRUE, promedio=TRUE, 
+                                 ratioavg=FALSE, ratiomax=FALSE)
+{
+  gc()
+  #Esta es la cantidad de meses que utilizo para la historia
+  ventana_regresion  <- ventana
+  
+  last  <- nrow( dataset )
+  
+  #creo el vector_desde que indica cada ventana
+  #de esta forma se acelera el procesamiento ya que lo hago una sola vez
+  vector_ids   <- dataset$numero_de_cliente
+  
+  vector_desde  <- seq( -ventana_regresion+2,  nrow(dataset)-ventana_regresion+1 )
+  vector_desde[ 1:ventana_regresion ]  <-  1
+  
+  for( i in 2:last )  if( vector_ids[ i-1 ] !=  vector_ids[ i ] ) {  vector_desde[i] <-  i }
+  for( i in 2:last )  if( vector_desde[i] < vector_desde[i-1] )  {  vector_desde[i] <-  vector_desde[i-1] }
+  
+  for(  campo  in   cols )
+  {
+    nueva_col     <- fhistC( dataset[ , get(campo) ], vector_desde ) 
+    
+    if(tendencia)  dataset[ , paste0( campo, "_tend", ventana) := nueva_col[ (0*last +1):(1*last) ]  ]
+    if(minimo)     dataset[ , paste0( campo, "_min", ventana)  := nueva_col[ (1*last +1):(2*last) ]  ]
+    if(maximo)     dataset[ , paste0( campo, "_max", ventana)  := nueva_col[ (2*last +1):(3*last) ]  ]
+    if(promedio)   dataset[ , paste0( campo, "_avg", ventana)  := nueva_col[ (3*last +1):(4*last) ]  ]
+    if(ratioavg)   dataset[ , paste0( campo, "_ratioavg", ventana)  := get(campo) /nueva_col[ (3*last +1):(4*last) ]  ]
+    if(ratiomax)   dataset[ , paste0( campo, "_ratiomax", ventana)  := get(campo) /nueva_col[ (2*last +1):(3*last) ]  ]
+  }
+  
+}
+#------------------------------------------------------------------------------
 #Autor: Antonio Velazquez Bustamente,  UBA 2021
 
 Tony  <- function( cols )
